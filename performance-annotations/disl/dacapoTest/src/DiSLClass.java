171cd45365599c28f3f9a550e69cf8339d8bbf4d
Irene Jacob <irene.jacob@usi.ch>
2017-05-01 15:41:11 +0200

    making basic graphs for lusearch

--- performance-annotations/disl/dacapoTest/src/DiSLClass.java
+++ performance-annotations/disl/dacapoTest/src/DiSLClass.java
@@ -1,55 +1,29 @@
-import Profiler.*;
-import ch.usi.dag.disl.annotation.*;
+import ch.usi.dag.disl.annotation.Before;
+import ch.usi.dag.disl.annotation.After;
 import ch.usi.dag.disl.marker.BodyMarker;
-import ch.usi.dag.disl.marker.Parameter;
-import ch.usi.dag.disl.processorcontext.ArgumentContext;
+import ch.usi.dag.disl.annotation.ThreadLocal;
+import java.util.Stack;
 import ch.usi.dag.disl.processorcontext.ArgumentProcessorContext;
 import ch.usi.dag.disl.processorcontext.ArgumentProcessorMode;
 import ch.usi.dag.disl.staticcontext.MethodStaticContext;
-import com.google.common.collect.ImmutableList;
-import com.google.javascript.jscomp.NodeTraversal;
-import com.google.javascript.jscomp.Scope;
-import com.google.javascript.jscomp.parsing.parser.Parser;
-import com.google.javascript.jscomp.parsing.parser.trees.ExpressionStatementTree;
-import com.google.javascript.jscomp.parsing.parser.trees.ParseTree;
-import com.google.javascript.jscomp.parsing.parser.trees.SwitchStatementTree;
-import com.google.javascript.rhino.Node;
+import profiler.IntArgumentProcessor;
 
-import java.util.ArrayList;
-import java.util.List;
+public class DiSLClass{
+    @ThreadLocal
+    static Stack<String> callStack;
 
-/**
- * Created by irenesjacob on 17.03.17.
- * Example instrumentation class.
- */
-
-
-public class DiSLClass {
-
-    @SyntheticLocal
-    static long entryTime;
-
-    @Before(marker = BodyMarker.class, scope = "com.google.javascript.jscomp.parsing.parser.Parser.parse", order = 1000)
-    public static void onMethodEntry(MethodStaticContext msc) {
-        entryTime = System.nanoTime();
-//        System.out.println(msc.thisMethodFullName());
+    @Before(marker=BodyMarker.class, scope="org.dacapo.luindex.*.*")
+    static void pushOnMethodEntry(MethodStaticContext msc){
+        System.out.println(msc.thisMethodFullName());
+        if (callStack==null) {
+            callStack = new Stack<String>();
+        }
+        callStack.push(msc.thisMethodFullName());
     }
 
-    @After(marker = BodyMarker.class, scope = "com.google.javascript.jscomp.parsing.parser.Parser.parse", order = 1000)
-    static void popOnMethodExit(MethodStaticContext msc, ArgumentProcessorContext apc) {
-        try {
-            String methodName = msc.thisMethodName();
-            long duration = System.nanoTime() - entryTime;
-//            String e = (String) apc.getArgs(ArgumentProcessorMode.METHOD_ARGS)[0].toString();
-            //            int feature = (int) apc.getArgs(ArgumentProcessorMode.METHOD_ARGS)[0];
-//            Node node = (Node) apc.getArgs(ArgumentProcessorMode.METHOD_ARGS)[1];
-//            feature = node.getChildCount();
-//            System.out.println(e);
-            ProfileData.addvalue("parse",Thread.currentThread(),duration);
-//            Profiler.addFeatureValuePair(feature, duration);
-        } catch (Exception e) {
-            System.out.println("An error "+ " ( " + (e) + " ) " + "occurred");
-        }
-
+    @After(marker=BodyMarker.class, scope="org.dacapo.luindex.*.*")
+    static void popOnMethodExit(ArgumentProcessorContext apc){
+//         apc.apply(IntArgumentProcessor.class, ArgumentProcessorMode.METHOD_ARGS);
+        callStack.pop();
     }
 }
---
+++ performance-annotations/disl/dacapoTest/src/profiler/ProfileExecutionTime.java
@@ -0,0 +1,40 @@
+package profiler;
+
+import java.util.ArrayList;
+import java.nio.file.Path;
+import java.nio.file.FileSystems;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by irenesjacob on 30.04.17.
+ */
+public class ProfileExecutionTime {
+   private ProfileExecutionTime(){/*Prevent instantiation*/}
+
+     private static final ArrayList<Long> cache = new ArrayList<>();
+     static {
+         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+             try{
+                 final Path completed = FileSystems.getDefault().getPath("./lucene","Index.dat");
+                 PrintWriter out = new PrintWriter(Files.newBufferedWriter(completed));
+                 for (Long val: cache) {
+                         out.println(val);
+                 }
+                 out.close();
+             }catch (IOException e){
+                 System.out.println(" hook called. Failed to write");
+             }
+         }));
+     }
+
+
+     public static void addValue(final long value){
+         cache.add(value);
+         // value can be execution time, bytecode executed etc.
+     }
+}
